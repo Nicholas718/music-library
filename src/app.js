027b27794de3870db24ad0ae3313bd1ae4d14439
Nicholas Barfoot <nbarfoot09@gmail.com>
2024-01-24 20:43:35 +0000

    added tests and have tested the test to ensure it works.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
@@ -6,6 +6,27 @@ app.use(express.json());
 
 app.get("/", (_req, res) => {
   res.send("Hello World!");
+});
+
+const artists = [];
+
+app.post("/artists", (req, res) => {
+  const { name, genre } = req.body;
+
+  if (!name || !genre) {
+    return res
+      .status(400)
+      .json({ error: "Name and genre are required fields" });
+  }
+
+  const newArtist = {
+    name,
+    genre,
+  };
+
+  artists.push(newArtist);
+
+  res.status(201).json(newArtist);
 });
 
 module.exports = app;
