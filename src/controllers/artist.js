56dcc838588a1228d1f3af399a2033e2166b2caf
Nicholas Barfoot <nbarfoot09@gmail.com>
2024-01-30 20:01:14 +0000

    Get a single artist function has been added.

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -41,4 +41,21 @@ const getAllArtists = async (_, res) => {
   }
 };
 
-module.exports = { createArtist, getAllArtists };
+const getArtistById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      rows: [artist],
+    } = await db.query("SELECT * FROM Artists WHERE id = $1", [id]);
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+module.exports = { createArtist, getAllArtists, getArtistById };
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -5,5 +5,6 @@ const artistRouter = express.Router();
 
 artistRouter.post("/artists", artistController.createArtist);
 artistRouter.get("/artists", artistController.getAllArtists);
+artistRouter.get("/artists/:id", artistController.getArtistById);
 
 module.exports = artistRouter;
